cba854684acfde003c77a0df9b29cacff5701c9f
larsme <larsme@web.de>
2020-12-13 12:41:31 +0100

    prepare version where argmax is replaced with nconv - out of memory

---
+++ modules/StructNConv/StructNConv2D_e_with_d_prod.py
@@ -0,0 +1,178 @@
+
+########################################
+__author__ = "Abdelrahman Eldesokey"
+__license__ = "GNU GPLv3"
+__version__ = "0.1"
+__maintainer__ = "Abdelrahman Eldesokey"
+__email__ = "abdo.eldesokey@gmail.com"
+########################################
+import torch
+import torch.nn.functional as F
+
+from modules.StructNConv.retrieve_indices import retrieve_indices
+
+class StructNConv2D_e_with_d(torch.nn.Module):
+    def __init__(self, init_method='k', mirror_weights=False, 
+                 in_channels=1, out_channels=1, groups=1, kernel_size=1, stride=1, padding=0, dilation=1, devalue_pooled_confidence=True):
+        super(StructNConv2D_e_with_d, self).__init__()
+
+        self.eps = 1e-20
+        self.init_method = init_method
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.groups = groups
+
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+
+        self.mirror_weights = mirror_weights
+        
+        self.devalue_conf = 1 / self.stride / self.stride if devalue_pooled_confidence else 1
+
+        # Define Parameters
+        self.w_prop = torch.nn.Parameter(data=torch.Tensor(1, self.in_channels, 1, 1, 1))
+        if mirror_weights:
+            self.w_s_from_d = torch.nn.Parameter(data=torch.Tensor(1, self.in_channels, 5, 1, 1))
+            self.spatial_weight0 = torch.nn.Parameter(data=torch.Tensor(self.out_channels, 1, self.kernel_size, self.kernel_size))
+            self.spatial_weight1 = torch.nn.Parameter(data=torch.Tensor(self.out_channels, 1, self.kernel_size, (self.kernel_size + 1) // 2))
+            self.spatial_weight3 = torch.nn.Parameter(data=torch.Tensor(self.out_channels, 1, self.kernel_size, (self.kernel_size + 1) // 2))
+            self.channel_weight = torch.nn.Parameter(data=torch.Tensor(self.out_channels, self.in_channels, 10))
+        else:
+            self.w_s_from_d = torch.nn.Parameter(data=torch.Tensor(1, self.in_channels, 2, 4, 1, 1))
+            self.spatial_weight = torch.nn.Parameter(data=torch.Tensor(self.out_channels * 4, 1, self.kernel_size, self.kernel_size))
+            self.channel_weight = torch.nn.Parameter(data=torch.Tensor(self.out_channels * 4, self.in_channels * 4, 1, 1))
+        
+        # Init Parameters
+        if self.init_method == 'x':  # Xavier
+            torch.nn.init.xavier_uniform_(self.w_s_from_d) + 2
+            torch.nn.init.xavier_uniform_(self.w_prop) + 1
+            torch.nn.init.xavier_uniform_(self.channel_weight) + 1
+            if mirror_weights:
+               torch.nn.init.xavier_uniform_(self.spatial_weight0) + 1
+               torch.nn.init.xavier_uniform_(self.spatial_weight1) + 1
+               torch.nn.init.xavier_uniform_(self.spatial_weight3) + 1
+            else:
+               torch.nn.init.xavier_uniform_(self.spatial_weight) + 1
+        else:  # elif self.init_method == 'k': # Kaiming
+            torch.nn.init.kaiming_uniform_(self.w_s_from_d) + 1
+            torch.nn.init.kaiming_uniform_(self.w_prop)
+            torch.nn.init.kaiming_uniform_(self.channel_weight)
+            if mirror_weights:
+               torch.nn.init.kaiming_uniform_(self.spatial_weight0)
+               torch.nn.init.kaiming_uniform_(self.spatial_weight1)
+               torch.nn.init.kaiming_uniform_(self.spatial_weight3)
+            else:
+                torch.nn.init.kaiming_uniform_(self.spatial_weight)
+        if mirror_weights:
+            self.spatial_weight0.data[:,:, self.kernel_size // 2, self.kernel_size // 2] = 1
+            self.spatial_weight1.data[:,:, self.kernel_size // 2, self.kernel_size // 2] = 1
+            self.spatial_weight3.data[:,:, self.kernel_size // 2, self.kernel_size // 2] = 1
+        else:
+            self.spatial_weight.data[:,:, self.kernel_size // 2, self.kernel_size // 2] = 1
+
+            
+    def enforce_limits(self):
+        # Enforce positive weights
+        if self.mirror_weights:
+            self.spatial_weight0.data = F.softplus(self.spatial_weight0, beta=10)
+            self.spatial_weight1.data = F.softplus(self.spatial_weight1, beta=10)
+            self.spatial_weight3.data = F.softplus(self.spatial_weight3, beta=10)
+        else:
+            self.spatial_weight.data = F.softplus(self.spatial_weight, beta=10)
+        self.channel_weight.data = F.softplus(self.channel_weight, beta=10)
+        self.w_prop.data = F.softplus(self.w_prop, beta=10)
+        self.w_s_from_d.data = F.softplus(self.w_s_from_d, beta=10)
+
+
+    def forward(self, d, cd, s, cs):
+        if self.mirror_weights:
+            self.spatial_weight = torch.cat((self.spatial_weight0,
+                                             torch.cat((self.spatial_weight1, self.spatial_weight1[:,:,:,:-1].flip(dims=(3,))), dim=3),
+                                             self.spatial_weight0.flip(dims=(3,)),
+                                             torch.cat((self.spatial_weight3, self.spatial_weight3[:,:,:,:-1].flip(dims=(3,))), dim=3)), dim=0)
+            # / -> |  = \  -> |
+            # / -> -  = \  -> -
+            # | -> /  = |  -> \
+            # - -> /  = -  -> \
+            # / -> /  = \  -> \
+            # / -> \  = \  -> /
+            channel_weight = torch.stack((torch.stack((self.channel_weight[:,:,0], self.channel_weight[:,:,1], self.channel_weight[:,:,2], self.channel_weight[:,:,3]), dim = 1),
+                                          torch.stack((self.channel_weight[:,:,4], self.channel_weight[:,:,5], self.channel_weight[:,:,4], self.channel_weight[:,:,6]), dim = 1),
+                                          torch.stack((self.channel_weight[:,:,2], self.channel_weight[:,:,1], self.channel_weight[:,:,0], self.channel_weight[:,:,3]), dim = 1),
+                                          torch.stack((self.channel_weight[:,:,7], self.channel_weight[:,:,8], self.channel_weight[:,:,7], self.channel_weight[:,:,9]), dim = 1)), 
+                                         dim=3).view(self.out_channels * 4, self.in_channels * 4, 1, 1)
+            w_s_from_d = torch.stack((torch.stack((self.w_s_from_d[:,:,0], self.w_s_from_d[:,:,1]), dim=2),
+                                      torch.stack((self.w_s_from_d[:,:,2], self.w_s_from_d[:,:,3]), dim=2),
+                                      torch.stack((self.w_s_from_d[:,:,1], self.w_s_from_d[:,:,0]), dim=2),
+                                      torch.stack((self.w_s_from_d[:,:,4], self.w_s_from_d[:,:,4]), dim=2)), dim=3)
+        else:
+            channel_weight = self.channel_weight
+            w_s_from_d = self.w_s_from_d
+
+
+
+        # calculate smoothness from depths
+        #edges:
+        #0 => /
+        #1 => -
+        #2 => \
+        #3 => |
+        # dim 2 in 0 to 4
+        real_shape = d.shape
+        d, cd = F.pad(d, (1,1,1,1)), F.pad(cd, (1,1,1,1))
+        shape = d.shape
+
+        if self.kernel_size == 3:
+            d_min = d_max = d
+            cd_min = cd_max = cd
+        else:
+            w_max = d * cd + self.eps
+            w_min = cd / (d + self.eps) + self.eps
+            w_max_denom = F.avg_pool2d(w_max, kernel_size=3, stride=1, padding=1)
+            w_min_denom = F.avg_pool2d(w_min, kernel_size=3, stride=1, padding=1)
+
+
+            d_min, d_max = F.avg_pool2d(d * w_min, kernel_size=3, stride=1, padding=1) / w_min_denom, F.avg_pool2d(d * w_max, kernel_size=3, stride=1, padding=1) / w_max_denom
+            cd_min, cd_max = F.avg_pool2d(cd * w_min, kernel_size=3, stride=1, padding=1) / w_min_denom,  F.avg_pool2d(cd * w_max, kernel_size=3, stride=1, padding=1) / w_max_denom
+
+        # d_min on one side divided by d_max on the other = low value => edge in the middle
+        # clamp is needed to prevent min > max, which can happen because they originate in different regions
+        # ordinarely, this would still maintain min <= overlap <= max, but because of ^, d_min might chooes an initialized value over the true min of 0
+        d_min_div_max = torch.clamp(torch.stack((torch.stack((d_min[:,:, :-2, :-2] / (d_max[:,:,2:  ,2:] + self.eps), d_max[:,:, :-2, :-2] / (d_min[:,:,2:  ,2:] + self.eps)), dim=2),        
+                                                 torch.stack((d_min[:,:, :-2,1:-1] / (d_max[:,:,2:  ,1:-1] + self.eps), d_max[:,:, :-2,1:-1] / (d_min[:,:,2:  ,1:-1] + self.eps)), dim=2),
+                                                 torch.stack((d_min[:,:, :-2,2:] / (d_max[:,:,2:  , :-2] + self.eps), d_max[:,:, :-2,2:] / (d_min[:,:,2:  , :-2] + self.eps)), dim=2),
+                                                 torch.stack((d_min[:,:,1:-1,2:] / (d_max[:,:,1:-1, :-2] + self.eps), d_max[:,:,1:-1,2:] / (d_min[:,:,1:-1, :-2] + self.eps)), dim=2)),
+                                                dim=3),self.eps,1).pow(w_s_from_d)
+
+        c_min_div_max = torch.stack((torch.stack((cd_min[:,:, :-2, :-2] * cd_max[:,:,2:  ,2:], cd_max[:,:, :-2, :-2] * cd_min[:,:,2:  ,2:]), dim=2),
+                                     torch.stack((cd_min[:,:, :-2,1:-1] * cd_max[:,:,2:  ,1:-1], cd_max[:,:, :-2,1:-1] * cd_min[:,:,2:  ,1:-1]), dim=2),
+                                     torch.stack((cd_min[:,:, :-2,2:] * cd_max[:,:,2:  , :-2], cd_max[:,:, :-2,2:] * cd_min[:,:,2:  , :-2]), dim=2),
+                                     torch.stack((cd_min[:,:,1:-1,2:] * cd_max[:,:,1:-1, :-2], cd_max[:,:,1:-1,2:] * cd_min[:,:,1:-1, :-2]), dim=2)), dim=3)
+        
+        w_min = c_min_div_max / (d_min_div_max + self.eps)
+        w_min = w_min / (w_min.sum(2, keepdim=True) + self.eps)
+
+        s_from_d = (w_min * d_min_div_max).sum(2)
+        cs_from_d = (w_min * c_min_div_max).sum(dim=2)
+
+        # combine with previous smoothness
+        s = ((self.w_prop * cs * s + 1 * cs_from_d * s_from_d) / (self.w_prop * cs + 1 * cs_from_d + self.eps)).view(real_shape[0],-1,real_shape[2], real_shape[3])
+        cs = ((self.w_prop * cs + 1 * cs_from_d) / (self.w_prop + 1)).view(real_shape[0],-1,real_shape[2], real_shape[3])
+
+        # Normalized Convolution along channel dimensions
+        nom = F.conv2d(cs * s, channel_weight, groups=self.groups)
+        denom = F.conv2d(cs, channel_weight, groups=self.groups)
+        s = (nom / (denom + self.eps))
+        cs = (denom / (torch.sum(channel_weight) + self.eps))
+
+        
+        # Normalized Convolution along spatial dimensions
+        nom = F.conv2d(cs * s, self.spatial_weight, groups=self.out_channels * 4, stride=self.stride, padding=self.padding, dilation=self.dilation).view(real_shape[0], self.out_channels, 4, real_shape[2], real_shape[3])
+        denom = F.conv2d(cs, self.spatial_weight, groups=self.out_channels * 4, stride=self.stride, padding=self.padding, dilation=self.dilation).view(real_shape[0], self.out_channels, 4, real_shape[2], real_shape[3])
+        s = nom / (denom + self.eps)
+        cs = denom / (torch.sum(self.spatial_weight) + self.eps)
+
+        return s, cs
---
+++ workspace/StructNConv/Unguided_de/Unguided_de_prod.py
@@ -0,0 +1,199 @@
+########################################
+__author__ = "Abdelrahman Eldesokey"
+__license__ = "GNU GPLv3"
+__version__ = "0.1"
+__maintainer__ = "Abdelrahman Eldesokey"
+__email__ = "abdo.eldesokey@gmail.com"
+########################################
+import torch
+
+#from modules.StructNConv.s_prod_KernelChannels import s_prod_KernelChannels
+from modules.StructNConv.e_prod_KernelChannels3 import e_prod_KernelChannels3
+from modules.StructNConv.e_prod_KernelChannels5_prod import e_prod_KernelChannels5
+
+from modules.StructNConv.StructNMaxPool2D_e_prod import StructNMaxPool2D_e
+from modules.StructNConv.StructNMaxPool2D_d_with_e import StructNMaxPool2D_d_with_e
+
+from modules.StructNConv.StructNConv2D_e_with_d_prod import StructNConv2D_e_with_d
+from modules.StructNConv.StructNConv2D_d_with_s import StructNConv2D_d_with_s
+from modules.StructNConv.StructNConv2D_d import StructNConv2D_d
+from modules.StructNConv.StructNConv2D_out import StructNConv2D_out
+
+from modules.StructNConv.StructNDeconv2D import StructNDeconv2D
+from modules.StructNConv.StructNDeconv2D_e import StructNDeconv2D_e
+from modules.StructNConv.NearestNeighbourUpsample import NearestNeighbourUpsample
+from modules.StructNConv.NearestNeighbourUpsample2 import NearestNeighbourUpsample2
+from modules.StructNConv.ReturnNone import ReturnNone
+
+
+class CNN(torch.nn.Module):
+
+    def __init__(self, params):
+        num_channels = params['num_channels']
+        devalue_pooled_confidence = params['devalue_pooled_confidence']
+
+        maxpool_e = params['maxpool_e']
+        nn_upsample_e = params['nn_upsample_e']
+
+        maxpool_d = params['maxpool_d']
+        nn_upsample_d = params['nn_upsample_d']
+
+        assert params['lidar_padding'] == 0
+        super().__init__()
+        
+        # boundary/smoothness modules
+        self.nconv_e = torch.nn.ModuleList([StructNConv2D_e_with_d(in_channels=1, out_channels=num_channels,init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=5, stride=1, padding=2, dilation=1),
+                        StructNConv2D_e_with_d(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=5, stride=1, padding=2, dilation=1),
+                        StructNConv2D_e_with_d(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=5, stride=1, padding=2, dilation=1),
+                        # pool
+                        StructNConv2D_e_with_d(in_channels=2 * num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=3, stride=1, padding=1, dilation=1),
+                        StructNConv2D_e_with_d(in_channels=2 * num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=3, stride=1, padding=1, dilation=1),
+                        StructNConv2D_e_with_d(in_channels=2 * num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=3, stride=1, padding=1, dilation=1)])
+        # pooling
+        if maxpool_e:
+            self.npool_e = StructNMaxPool2D_e(channels=num_channels,
+                                              kernel_size=3, stride=2, padding=1,
+                                              devalue_pooled_confidence=devalue_pooled_confidence)
+        else:
+            self.npool_e = StructNConv2D_e_with_d(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                                  kernel_size=3, stride=2, padding=1, dilation=1,
+                                                  devalue_pooled_confidence=devalue_pooled_confidence)
+        if nn_upsample_e:
+            self.nup_e = NearestNeighbourUpsample2(kernel_size=3, stride=2, padding=1)
+        else:
+            self.nup_e = StructNDeconv2D_e(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                                  kernel_size=3, stride=2, padding=1, dilation=1)
+
+
+        # e_prod
+        self.e_prod_1 = e_prod_KernelChannels5()
+        if maxpool_d:
+            self.e_prod_pool = ReturnNone()
+        else:
+            self.e_prod_pool = e_prod_KernelChannels3(stride=2)
+        self.e_prod_2 = e_prod_KernelChannels3()
+
+
+        # depth modules
+        # in_channels not 1 because of multiplication with output of nconv1_e
+        self.nconv_d = torch.nn.ModuleList([StructNConv2D_d_with_s(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=5, stride=1, padding=2, dilation=1),
+                        StructNConv2D_d_with_s(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=5, stride=1, padding=2, dilation=1),
+                        StructNConv2D_d_with_s(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=5, stride=1, padding=2, dilation=1),
+                        # pool
+                        StructNConv2D_d_with_s(in_channels=2 * num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=3, stride=1, padding=1, dilation=1),
+                        StructNConv2D_d_with_s(in_channels=2 * num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=3, stride=1, padding=1, dilation=1),
+                        StructNConv2D_d_with_s(in_channels=2 * num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                               kernel_size=3, stride=1, padding=1, dilation=1),
+                        # out
+                        StructNConv2D_out(in_channels=num_channels, init_method=params['init_method'])])
+        # pooling
+        if maxpool_d:
+            self.npool_d = StructNMaxPool2D_d_with_e(kernel_size=3, stride=2, padding=1)
+        else:
+            self.npool_d = StructNConv2D_d_with_s(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                                  kernel_size=3, stride=2, padding=1, dilation=1)
+        if nn_upsample_d:
+            self.nup_d = NearestNeighbourUpsample(kernel_size=3, stride=2, padding=1)
+        else:
+            self.nup_d = StructNDeconv2D(in_channels=num_channels, out_channels=num_channels, init_method=params['init_method'], mirror_weights=params['mirror_weights'],
+                                                  kernel_size=3, stride=2, padding=1, dilation=1)
+        self.outs = ['d', 'cd', 'e', 'ce']
+
+        self.enforce_limits()
+
+    def enforce_limits(self):
+        for nconv in self.nconv_d:
+            nconv.enforce_limits()
+        for nconv in self.nconv_e:
+            nconv.enforce_limits()
+        self.npool_d.enforce_limits()
+        self.nup_d.enforce_limits()
+        self.npool_e.enforce_limits()
+        self.nup_e.enforce_limits()
+    
+    
+    def forward(self, d_0, cd_0, e_0=None, ce_0 = None):
+        if e_0 is None:
+            e_0 = ce_0 = torch.zeros(size=(d_0.shape[0], d_0.shape[1], 4, d_0.shape[2], d_0.shape[3]), device=d_0.device)        
+
+        # Stage 0
+        e_0, ce_0 = self.nconv_e[0](d_0, cd_0, e_0, ce_0)
+        e_prod = self.e_prod_1(e_0, ce_0)
+        d_0, cd_0 = self.nconv_d[0](d_0, cd_0, e_0, ce_0, e_prod)
+        e_0, ce_0 = self.nconv_e[1](d_0, cd_0, e_0, ce_0)
+        e_prod = self.e_prod_1(e_0, ce_0)
+        d_0, cd_0 = self.nconv_d[1](d_0, cd_0, e_0, ce_0, e_prod)
+        e_0, ce_0 = self.nconv_e[2](d_0, cd_0, e_0, ce_0)
+        e_prod = self.e_prod_1(e_0, ce_0)
+        d_0, cd_0 = self.nconv_d[2](d_0, cd_0, e_0, ce_0, e_prod)
+
+        # Stage 1
+        e_1, ce_1 = self.npool_e(d_0, cd_0, e_0, ce_0)
+        e_prod = self.e_prod_pool(e_0, ce_0)
+        d_1, cd_1 = self.npool_d(d_0, cd_0, e_0, ce_0, e_prod)
+        e_1, ce_1 = self.nconv_e[1](d_1, cd_1, e_1, ce_1)
+        e_prod = self.e_prod_1(e_1, ce_1)
+        d_1, cd_1 = self.nconv_d[1](d_1, cd_1, e_1, ce_1, e_prod)
+        e_1, ce_1 = self.nconv_e[2](d_1, cd_1, e_1, ce_1)
+        e_prod = self.e_prod_1(e_1, ce_1)
+        d_1, cd_1 = self.nconv_d[2](d_1, cd_1,  e_1, ce_1, e_prod)
+
+        # Stage 2
+        e_2, ce_2 = self.npool_e(d_1, cd_1, e_1, ce_1)
+        e_prod = self.e_prod_pool(e_1, ce_1)
+        d_2, cd_2 = self.npool_d(d_1, cd_1, e_1, ce_1, e_prod)
+        e_2, ce_2 = self.nconv_e[1](d_2, cd_2, e_2, ce_2)
+        e_prod = self.e_prod_1(e_2, ce_2)
+        d_2, cd_2 = self.nconv_d[1](d_2, cd_2, e_2, ce_2, e_prod)
+
+        # Stage 3
+        e_3, ce_3 = self.npool_e(d_2, cd_2, e_2, ce_2)
+        e_prod = self.e_prod_pool(e_2, ce_2)
+        d_3, cd_3 = self.npool_d(d_2, cd_2, e_2, ce_2, e_prod)
+        e_3, ce_3 = self.nconv_e[1](d_3, cd_3, e_3, ce_3)
+        e_prod = self.e_prod_1(e_3, ce_3)
+        d_3, cd_3 = self.nconv_d[1](d_3, cd_3, e_3, ce_3, e_prod)
+
+        # Stage 2
+        e_32, ce_32 = self.nup_e(e_3, ce_3, e_2.shape)
+        d_32, cd_32 = self.nup_d(d_3, cd_3, d_2.shape)
+        e_2, ce_2 = torch.cat((e_32, e_2), 1), torch.cat((ce_32, ce_2), 1)
+        d_2, cd_2 = torch.cat((d_32, d_2), 1), torch.cat((cd_32, cd_2), 1)
+        e_2, ce_2 = self.nconv_e[3](d_2, cd_2, e_2, ce_2)
+        e_prod = self.e_prod_2(e_2, ce_2)
+        d_2, cd_2 = self.nconv_d[3](d_2, cd_2, e_2, ce_2, e_prod)
+
+        # Stage 1
+        e_21, ce_21 = self.nup_e(e_2, ce_2, e_1.shape)
+        d_21, cd_21 = self.nup_d(d_2, cd_2, d_1.shape)
+        e_1, ce_1 = torch.cat((e_21, e_1), 1), torch.cat((ce_21, ce_1), 1)
+        d_1, cd_1 = torch.cat((d_21, d_1), 1), torch.cat((cd_21, cd_1), 1)
+        e_1, ce_1 = self.nconv_e[4](d_1, cd_1, e_1, ce_1)
+        e_prod = self.e_prod_2(e_1, ce_1)
+        d_1, cd_1 = self.nconv_d[4](d_1, cd_1, e_1, ce_1, e_prod)
+
+        # Stage 0
+        e_10, ce_10 = self.nup_e(e_1, ce_1, e_0.shape)
+        d_10, cd_10 = self.nup_d(d_1, cd_1, d_0.shape)
+        e_0, ce_0 = torch.cat((e_10, e_0), 1), torch.cat((ce_10, ce_0), 1)
+        d_0, cd_0 = torch.cat((d_10, d_0), 1), torch.cat((cd_10, cd_0), 1)
+        e_0, ce_0 = self.nconv_e[5](d_0, cd_0, e_0, ce_0)
+        e_prod = self.e_prod_2(e_0, ce_0)
+        d_0, cd_0 = self.nconv_d[5](d_0, cd_0, e_0, ce_0, e_prod)
+
+        # output
+        d, cd = self.nconv_d[6](d_0, cd_0)
+        s, cs = self.nconv_d[6](e_0.view(d.shape[0], -1, d.shape[2]*4, d.shape[3]), ce_0.view(d.shape[0], -1, d.shape[2]*4, d.shape[3]))
+        s[cs == 0] = 0
+        return d, cd, s.view(d.shape[0], 4, d.shape[2], d.shape[3]), cs.view(d.shape[0], 4, d.shape[2], d.shape[3])
